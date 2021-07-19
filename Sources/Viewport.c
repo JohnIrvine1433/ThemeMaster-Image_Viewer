034a6d8d2c7a31def4f8a73e34e85a603faf96ee
JohnIrvine1433 <john.irvine.doe@gmail.com>
2021-07-19 09:12:31 +0200

    Update Viewport.c

--- Sources/Viewport.c
+++ Sources/Viewport.c
@@ -49,14 +49,14 @@ int ViewportAdaptImage(void)
 	unsigned int Pixel_Format;
 	int Original_Image_Width, Original_Image_Height, Horizontal_Scaling_Percentage, Vertical_Scaling_Percentage;
 	SDL_Rect Rectangle_Original_Image_Dimensions;
-	
+
 	// Get original image dimensions (these values could be kept as global, but SDL_QueryTexture() is really fast)
 	if (SDL_QueryTexture(Pointer_Viewport_Texture_Original_Image, &Pixel_Format, NULL, &Original_Image_Width, &Original_Image_Height) != 0)
 	{
 		printf("[%s:%d] Error : failed to query original image texture parameters (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-		
+
 	// Adjust image size to viewport ratio to make sure the image will keep its ratio
 	// Image is smaller than the viewport, keep viewport size
 	if ((Original_Image_Width < Viewport_Width) && (Original_Image_Height < Viewport_Height))
@@ -70,7 +70,7 @@ int ViewportAdaptImage(void)
 		// Determine which size (horizontal or vertical) must be filled to keep the ratio (all computations are made with percentages to be easier to understand, and also because multiplying by 100 allows simple and fast fixed calculations with accurate enough results)
 		Horizontal_Scaling_Percentage = (100 * Original_Image_Width) / Viewport_Width; // Find how much the image is larger than the viewport
 		Vertical_Scaling_Percentage = (100 * Original_Image_Height) / Viewport_Height; // Find how much the image is higher than the viewport
-		
+
 		// Horizontal size is more "compressed" than vertical one for this viewport, so display the full image horizontal size and adjust vertical size to keep ratio
 		if (Horizontal_Scaling_Percentage > Vertical_Scaling_Percentage)
 		{
@@ -84,10 +84,10 @@ int ViewportAdaptImage(void)
 			Viewport_Adjusted_Image_Width = (Viewport_Width * Vertical_Scaling_Percentage) / 100; // Fill the image right to keep the ratio
 		}
 	}
-	
+
 	// Remove the previously existing texture
 	if (Pointer_Viewport_Texture_Adapted_Image != NULL) SDL_DestroyTexture(Pointer_Viewport_Texture_Adapted_Image);
-	
+
 	// Create a texture with the right dimensions to keep the image ratio
 	Pointer_Viewport_Texture_Adapted_Image = SDL_CreateTexture(Pointer_Viewport_Window_Renderer, Pixel_Format, SDL_TEXTUREACCESS_TARGET, Viewport_Adjusted_Image_Width, Viewport_Adjusted_Image_Height);
 	if (Pointer_Viewport_Texture_Adapted_Image == NULL)
@@ -95,7 +95,7 @@ int ViewportAdaptImage(void)
 		printf("[%s:%d] Error : failed to create the adjusted image texture (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Fill the texture with a visible background color, so the original image dimensions are easily visible
 	if (SDL_SetRenderTarget(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Adapted_Image) != 0)
 	{
@@ -114,28 +114,29 @@ int ViewportAdaptImage(void)
 		printf("[%s:%d] Error : failed to fill the adjusted image texture with the background color (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Copy the original image on the adjusted image
 	Rectangle_Original_Image_Dimensions.x = 0;
 	Rectangle_Original_Image_Dimensions.y = 0;
 	Rectangle_Original_Image_Dimensions.w = Original_Image_Width - 1;
 	Rectangle_Original_Image_Dimensions.h = Original_Image_Height - 1;
-	if (SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Original_Image, NULL, &Rectangle_Original_Image_Dimensions, 0, NULL, Viewport_Adapted_Image_Flipping_Mode) != 0)
+	//if (SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Original_Image, NULL, &Rectangle_Original_Image_Dimensions, 0, NULL, Viewport_Adapted_Image_Flipping_Mode) != 0)
+	if (SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Original_Image, NULL, NULL, 0, NULL, Viewport_Adapted_Image_Flipping_Mode) != 0)
 	{
 		printf("[%s:%d] Error : failed to render the original image texture on the adapted image texture (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Restore the renderer (it can write to the display again)
 	if (SDL_SetRenderTarget(Pointer_Viewport_Window_Renderer, NULL) != 0)
 	{
 		printf("[%s:%d] Error : failed to target the renderer to the display (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Reset zoom when resizing the window to avoid aiming to whatever but the right place
 	ViewportSetZoomedArea(0, 0, 1);
-	
+
 	return 0;
 }
 
@@ -151,7 +152,7 @@ int ViewportInitialize(char *String_Window_Title, SDL_Surface *Pointer_Surface_I
 		printf("[%s:%d] Error : failed to create the SDL window (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Try to create an hardware-accelerated renderer to plug to the window
 	Pointer_Viewport_Window_Renderer = SDL_CreateRenderer(Pointer_Viewport_Window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	if (Pointer_Viewport_Window_Renderer == NULL)
@@ -159,7 +160,7 @@ int ViewportInitialize(char *String_Window_Title, SDL_Surface *Pointer_Surface_I
 		printf("[%s:%d] Error : failed to create the SDL window renderer (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Convert the image surface to a texture
 	Pointer_Viewport_Texture_Original_Image = SDL_CreateTextureFromSurface(Pointer_Viewport_Window_Renderer, Pointer_Surface_Image);
 	if (Pointer_Viewport_Texture_Original_Image == NULL)
@@ -167,16 +168,17 @@ int ViewportInitialize(char *String_Window_Title, SDL_Surface *Pointer_Surface_I
 		printf("[%s:%d] Error : failed to convert the image surface to a texture (%s).\n", __FUNCTION__, __LINE__, SDL_GetError());
 		return -1;
 	}
-	
+
 	// Do not allow the window to be too small because it can prevent the texture rendering from working
 	SDL_SetWindowMinimumSize(Pointer_Viewport_Window, CONFIGURATION_VIEWPORT_MINIMUM_WINDOW_WIDTH, CONFIGURATION_VIEWPORT_MINIMUM_WINDOW_HEIGHT);
-	
+
 	return 0;
 }
 
 void ViewportDrawImage(void)
 {
-	SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Adapted_Image, &Viewport_Rectangle_View, NULL, 0, NULL, 0);
+	//SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Adapted_Image, &Viewport_Rectangle_View, NULL, 0, NULL, 0);
+	SDL_RenderCopyEx(Pointer_Viewport_Window_Renderer, Pointer_Viewport_Texture_Adapted_Image, NULL, NULL, 0, NULL, 0);
 	SDL_RenderPresent(Pointer_Viewport_Window_Renderer);
 }
 
@@ -185,7 +187,7 @@ void ViewportSetDimensions(int New_Viewport_Width, int New_Viewport_Height)
 	// Store new viewport dimensions
 	Viewport_Width = New_Viewport_Width;
 	Viewport_Height = New_Viewport_Height;
-	
+
 	// Add additionnal borders to the image to keep its ratio
 	ViewportAdaptImage();
 }
@@ -194,10 +196,10 @@ void ViewportSetZoomedArea(int Viewport_X, int Viewport_Y, int Zoom_Factor)
 {
 	int Rectangle_X = 0, Rectangle_Y = 0;
 	static int Previous_Zoom_Level_Rectangle_X = 0, Previous_Zoom_Level_Rectangle_Y = 0, Previous_Zoom_Factor = 1;
-	
+
 	// Do not compute viewing area once more if the maximum zooming level has been reached, because values would overflow
 	if ((Previous_Zoom_Factor == CONFIGURATION_VIEWPORT_MAXIMUM_ZOOM_FACTOR) && (Zoom_Factor == CONFIGURATION_VIEWPORT_MAXIMUM_ZOOM_FACTOR)) return;
-	
+
 	// Force the view rectangle to start from the viewport origin when there is no zooming
 	if (Zoom_Factor == 1)
 	{
@@ -216,19 +218,19 @@ void ViewportSetZoomedArea(int Viewport_X, int Viewport_Y, int Zoom_Factor)
 		Rectangle_X = Previous_Zoom_Level_Rectangle_X - (((Viewport_X / Previous_Zoom_Factor) * Viewport_Adjusted_Image_Width) / Viewport_Width);
 		Rectangle_Y = Previous_Zoom_Level_Rectangle_Y - (((Viewport_Y / Previous_Zoom_Factor) * Viewport_Adjusted_Image_Height) / Viewport_Height);
 	}
-	
+
 	// Make sure no negative coordinates are generated
 	if (Rectangle_X < 0) Rectangle_X = 0;
 	if (Rectangle_Y < 0) Rectangle_Y = 0;
-	
+
 	// There is nothing to do when zooming to 1x because the for loop will immediately exit and x and y coordinates will be set to 0
 	Viewport_Rectangle_View.x = Rectangle_X;
 	Viewport_Rectangle_View.y = Rectangle_Y;
-	
+
 	// The smaller the rectangle is, the more the image will be zoomed
 	Viewport_Rectangle_View.w = (Viewport_Adjusted_Image_Width / Zoom_Factor) - 1;
 	Viewport_Rectangle_View.h = (Viewport_Adjusted_Image_Height / Zoom_Factor) - 1;
-	
+
 	Previous_Zoom_Level_Rectangle_X = Rectangle_X;
 	Previous_Zoom_Level_Rectangle_Y = Rectangle_Y;
 	Previous_Zoom_Factor = Zoom_Factor;
@@ -242,24 +244,24 @@ void ViewportSetFlippingMode(TViewportFlippingModeID Flipping_Mode_ID)
 		case VIEWPORT_FLIPPING_MODE_ID_NORMAL:
 			Viewport_Adapted_Image_Flipping_Mode = SDL_FLIP_NONE;
 			break;
-			
+
 		case VIEWPORT_FLIPPING_MODE_ID_HORIZONTAL:
 			Viewport_Adapted_Image_Flipping_Mode = SDL_FLIP_HORIZONTAL;
 			break;
-			
+
 		case VIEWPORT_FLIPPING_MODE_ID_VERTICAL:
 			Viewport_Adapted_Image_Flipping_Mode = SDL_FLIP_VERTICAL;
 			break;
-			
+
 		case VIEWPORT_FLIPPING_MODE_ID_HORIZONTAL_AND_VERTICAL:
 			Viewport_Adapted_Image_Flipping_Mode = SDL_FLIP_HORIZONTAL | SDL_FLIP_VERTICAL;
 			break;
-			
+
 		default:
 			printf("[%s:%d] Error : bad flipping mode ID provided (%d).\n", __FUNCTION__, __LINE__, Flipping_Mode_ID);
 			return;
 	}
-	
+
 	// Redraw the image with the newly selected flipping mode
 	ViewportAdaptImage();
 }
